3b65832717e7ee4d08df3925264489f180172521
corey <corey@corey-MS-7B45.lan>
2022-05-05 14:06:23 -0700

    updated directory initializer and acquision parameter transfer scripts for docker compatability

--- Pipeline/bin/DirectoryInitializer_Sub.sh
+++ Pipeline/bin/DirectoryInitializer_Sub.sh
@@ -12,11 +12,11 @@ mkdir /opt/Pipeline/Pipeline/Pipeline/Stg1Tmp
 mkdir /opt/Pipeline/Pipeline/Pipeline/Stg2Tmp
 mkdir /opt/Pipeline/Pipeline/Pipeline/Stg3Tmp
 mkdir /opt/Pipeline/Pipeline/Pipeline/Stg4Tmp
-mkdir /opt/Pipeline/Pipeline/Pipeline/$1
-mkdir /opt/Pipeline/Pipeline/Pipeline/$2
-mkdir /opt/Pipeline/Pipeline/Pipeline/$3
-mkdir /opt/Pipeline/Pipeline/Pipeline/$4
-mkdir /opt/Pipeline/Pipeline/Pipeline/$5
+#mkdir /opt/Pipeline/Pipeline/Pipeline/$1
+#mkdir /opt/Pipeline/Pipeline/Pipeline/$2
+#mkdir /opt/Pipeline/Pipeline/Pipeline/$3
+#mkdir /opt/Pipeline/Pipeline/Pipeline/$4
+#mkdir /opt/Pipeline/Pipeline/Pipeline/$5
 while [ "$(( i += 1 ))" -le $arrSize ]
 do
 zi=$( printf '%02d' "$i")
@@ -26,11 +26,11 @@ mkdir /opt/Pipeline/Pipeline/Pipeline/Stg2Tmp/sub_$zi/
 mkdir /opt/Pipeline/Pipeline/Pipeline/Stg3Tmp/sub_$zi/
 mkdir /opt/Pipeline/Pipeline/Pipeline/Stg4Tmp/sub_$zi/
 mkdir /opt/Pipeline/Pipeline/Pipeline/Stg4Tmp/sub_$zi/BedpostX_$zi/
-mkdir $1/sub_$zi/  #denoised, degibbsed, merged images
-mkdir $2/sub_$zi/  #outputs of topup and eddy [corrected for EPI distortions, eddy currents, and subject movement]
-mkdir $3/sub_$zi/  #Anatomical and 0-Diffusion masks and 'nodif_brain.nii.gz'
-mkdir $4/sub_$zi/  #BedpostX outputs: Diffusion tensor estimates [Fractional Anisotropy and Mean Diffusivity]	
-mkdir $5/sub_$zi/ #Tractography outputs [fiber density norm, length, etc.]
+mkdir /$1/sub_$zi/  #denoised, degibbsed, merged images
+mkdir /$2/sub_$zi/  #outputs of topup and eddy [corrected for EPI distortions, eddy currents, and subject movement]
+mkdir /$3/sub_$zi/  #Anatomical and 0-Diffusion masks and 'nodif_brain.nii.gz'
+mkdir /$4/sub_$zi/  #BedpostX outputs: Diffusion tensor estimates [Fractional Anisotropy and Mean Diffusivity]	
+mkdir /$5/sub_$zi/ #Tractography outputs [fiber density norm, length, etc.]
 done
 echo "Directories initialized"
 
--- Pipeline/bin/min_M_acqp_initializer.sh
+++ Pipeline/bin/min_M_acqp_initializer.sh
@@ -8,7 +8,7 @@ while [ "$(( i += 1 ))" -le $arrSize ]
 do
 zi=$( printf '%02d' "$i")
 ReadOut=`grep TotalReadoutTime $1/sub_$zi/dwi/sub-"$zi"_acq-dir107_dir-PA_dwi.json | cut -c23-31`
-sed 's/$/ '$ReadOut'/' /opt/Pipeline/Pipeline/Pipeline/ACQP/ap_template_min.txt > /opt/Pipeline/Pipeline/Pipeline/Pipeline/ACQP/sub_"$zi"/acquisition_parameters_min.txt
+docker exec "dwi-pipeline3" sed 's/$/ '$ReadOut'/' /opt/Pipeline/Pipeline/Pipeline/ACQP/ap_template_min.txt > /opt/Pipeline/Pipeline/Pipeline/Pipeline/ACQP/sub_"$zi"/acquisition_parameters_min.txt
 cp $1/sub_$zi/dwi/sub-"$zi"_acq-dir107_dir-PA_dwi.bval /opt/Pipeline/Pipeline/Pipeline/Pipeline/ACQP/sub_$zi/bval
 cp $1/sub_$zi/dwi/sub-"$zi"_acq-dir107_dir-PA_dwi.bvec /opt/Pipeline/Pipeline/Pipeline/Pipeline/ACQP/sub_$zi/bvec
 done
