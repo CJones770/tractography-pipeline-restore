3fc1012a375a9a4500a7da6fee808f26856c0692
corey <corey@corey-MS-7B45.lan>
2022-05-05 08:52:34 -0700

    testing working directory in container with DirectoryInitializer.sh

--- Pipeline/bin/DirectoryInitializer_Sub.sh
+++ Pipeline/bin/DirectoryInitializer_Sub.sh
@@ -8,24 +8,24 @@ SubjectDirectory=$6
 arrSize=`ls $6 | wc -l`
 i=0
 echo "Initializing Directories for $arrSize subjects."
-mkdir ~/Pipeline/Stg1Tmp
-mkdir ~/Pipeline/Stg2Tmp
-mkdir ~/Pipeline/Stg3Tmp
-mkdir ~/Pipeline/Stg4Tmp
-mkdir $1
-mkdir $2
-mkdir $3
-mkdir $4
-mkdir $5
+mkdir /opt/Pipeline/Pipeline/Pipeline/Stg1Tmp
+mkdir /opt/Pipeline/Pipeline/Pipeline/Stg2Tmp
+mkdir /opt/Pipeline/Pipeline/Pipeline/Stg3Tmp
+mkdir /opt/Pipeline/Pipeline/Pipeline/Stg4Tmp
+mkdir /opt/Pipeline/Pipeline/Pipeline/$1
+mkdir /opt/Pipeline/Pipeline/Pipeline/$2
+mkdir /opt/Pipeline/Pipeline/Pipeline/$3
+mkdir /opt/Pipeline/Pipeline/Pipeline/$4
+mkdir /opt/Pipeline/Pipeline/Pipeline/$5
 while [ "$(( i += 1 ))" -le $arrSize ]
 do
 zi=$( printf '%02d' "$i")
-mkdir ~/Pipeline/ACQP/sub_$zi/
-mkdir ~/Pipeline/Stg1Tmp/sub_$zi/
-mkdir ~/Pipeline/Stg2Tmp/sub_$zi/ 
-mkdir ~/Pipeline/Stg3Tmp/sub_$zi/
-mkdir ~/Pipeline/Stg4Tmp/sub_$zi/
-mkdir ~/Pipeline/Stg4Tmp/sub_$zi/BedpostX_$zi/
+mkdir /opt/Pipeline/Pipeline/Pipeline/ACQP/sub_$zi/
+mkdir /opt/Pipeline/Pipeline/Pipeline/Stg1Tmp/sub_$zi/
+mkdir /opt/Pipeline/Pipeline/Pipeline/Stg2Tmp/sub_$zi/ 
+mkdir /opt/Pipeline/Pipeline/Pipeline/Stg3Tmp/sub_$zi/
+mkdir /opt/Pipeline/Pipeline/Pipeline/Stg4Tmp/sub_$zi/
+mkdir /opt/Pipeline/Pipeline/Pipeline/Stg4Tmp/sub_$zi/BedpostX_$zi/
 mkdir $1/sub_$zi/  #denoised, degibbsed, merged images
 mkdir $2/sub_$zi/  #outputs of topup and eddy [corrected for EPI distortions, eddy currents, and subject movement]
 mkdir $3/sub_$zi/  #Anatomical and 0-Diffusion masks and 'nodif_brain.nii.gz'
