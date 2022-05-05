5451b7fe7e6d776b05bf17b45b1934b5b3ec1fd1
corey <corey@corey-MS-7B45.lan>
2022-05-05 16:06:47 -0700

    tinkering with acquisition parameter transfer script

--- Pipeline/bin/min_M_acqp_initializer.sh
+++ Pipeline/bin/min_M_acqp_initializer.sh
@@ -8,8 +8,8 @@ while [ "$(( i += 1 ))" -le $arrSize ]
 do
 zi=$( printf '%02d' "$i")
 ReadOut=`grep TotalReadoutTime $1/sub_$zi/dwi/sub-"$zi"_acq-dir107_dir-PA_dwi.json | cut -c23-31`
-docker exec "dwi-pipeline3" sed 's/$/ '$ReadOut'/' /opt/Pipeline/Pipeline/Pipeline/ACQP/ap_template_min.txt > /opt/Pipeline/Pipeline/Pipeline/Pipeline/ACQP/sub_"$zi"/acquisition_parameters_min.txt
-cp $1/sub_$zi/dwi/sub-"$zi"_acq-dir107_dir-PA_dwi.bval /opt/Pipeline/Pipeline/Pipeline/Pipeline/ACQP/sub_$zi/bval
-cp $1/sub_$zi/dwi/sub-"$zi"_acq-dir107_dir-PA_dwi.bvec /opt/Pipeline/Pipeline/Pipeline/Pipeline/ACQP/sub_$zi/bvec
+docker exec "dwi-pipeline3" sed bash -c "echo 's/$/ '$ReadOut'/' /opt/Pipeline/Pipeline/Pipeline/ACQP/ap_template_min.txt > /opt/Pipeline/Pipeline/Pipeline/Pipeline/ACQP/sub_"$zi"/acquisition_parameters_min.txt"
+docker cp $1/sub_$zi/dwi/sub-"$zi"_acq-dir107_dir-PA_dwi.bval /opt/Pipeline/Pipeline/Pipeline/Pipeline/ACQP/sub_$zi/bval
+docker cp $1/sub_$zi/dwi/sub-"$zi"_acq-dir107_dir-PA_dwi.bvec /opt/Pipeline/Pipeline/Pipeline/Pipeline/ACQP/sub_$zi/bvec
 done
 echo "Acquisition parameters initialized. Bvec and Bval files copied to ~/Pipeline/ACQP/subject directories."
