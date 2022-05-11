e643fed9d14910535ad7c4859e68e3f3845b3438
corey <corey@corey-MS-7B45.lan>
2022-05-10 19:14:47 -0700

    remove unnecessary path to b02b0.cnf

--- Pipeline/bin/min_M_topup.sh
+++ Pipeline/bin/min_M_topup.sh
@@ -10,7 +10,7 @@ while [ "$(( i += 1 ))" -le $arrSize ]
 do
 zi=$( printf '%02d' "$i")
 topup --imain=/opt/Pipeline/Pipeline/Pipeline/Stg1Tmp/sub_$zi/AP_PA_TopupInput2Vols.nii.gz --datain=/opt/Pipeline/Pipeline/Pipeline/ACQP/sub_$zi/acquisition_parameters_min.txt --out=/opt/Pipeline/Pipeline/Pipeline/Stg2Tmp/sub_$zi/DNDGTopup \
---fout=/opt/Pipeline/Pipeline/Pipeline/Stg2Tmp/sub_$zi/DNDGTopup_field --iout=/opt/Pipeline/Pipeline/Pipeline/Stg2Tmp/sub_$zi/DNDGTopup_unwarped_images --config=/usr/local/fsl/etc/flirtsch/b02b0.cnf -v #--config=/opt/fsl/etc/flirtsch/b02b0.cnf
+--fout=/opt/Pipeline/Pipeline/Pipeline/Stg2Tmp/sub_$zi/DNDGTopup_field --iout=/opt/Pipeline/Pipeline/Pipeline/Stg2Tmp/sub_$zi/DNDGTopup_unwarped_images --config=b02b0.cnf -v #--config=/opt/fsl/etc/flirtsch/b02b0.cnf
 echo "Subject $zi complete"
 now=$(date +"%T")
 echo "Current time : $now"
