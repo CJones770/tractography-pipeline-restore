ffba6f7443b7286052302ec13acbce46d7097fe1
corey <corey@corey-MS-7B45.lan>
2022-05-10 22:40:23 -0700

    commented broken residual storage in Tmp1_2_Out, QC in eddy_quad

--- Pipeline/bin/min_M_Tmp1_2_Out.sh
+++ Pipeline/bin/min_M_Tmp1_2_Out.sh
@@ -9,8 +9,8 @@ echo "Copying Stage 1 Temp folder contents to $2 ..."
 while [ "$(( i += 1 ))" -le $arrSize ]
 do
 zi=$( printf '%02d' "$i")
-cp /opt/Pipeline/Pipeline/Pipeline/Stg1Tmp/sub_$zi/PA_residual.nii.gz $2/sub_$zi/PA_residual.nii.gz	
-cp /opt/Pipeline/Pipeline/Pipeline/Stg1Tmp/sub_$zi/AP_residual.nii.gz $2/sub_$zi/AP_residual.nii.gz
+#cp /opt/Pipeline/Pipeline/Pipeline/Stg1Tmp/sub_$zi/PA_residual.nii.gz $2/sub_$zi/PA_residual.nii.gz	
+#cp /opt/Pipeline/Pipeline/Pipeline/Stg1Tmp/sub_$zi/AP_residual.nii.gz $2/sub_$zi/AP_residual.nii.gz
 cp /opt/Pipeline/Pipeline/Pipeline/Stg1Tmp/sub_$zi/d107-AP_DNDG.nii.gz $2/sub_$zi/AP_DNDG.nii.gz
 cp /opt/Pipeline/Pipeline/Pipeline/Stg1Tmp/sub_$zi/d107-PA_DNDG.nii.gz $2/sub_$zi/PA_DNDG.nii.gz
 cp /opt/Pipeline/Pipeline/Pipeline/Stg1Tmp/sub_$zi/AP_PA_TopupInput2Vols.nii.gz $2/sub_$zi/merged_best_b0s.nii.gz
